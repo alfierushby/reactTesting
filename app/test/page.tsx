90a158144fcdd447de40be5f54e9aa70b5243e4f
ickeyben123 <rushbyalfie99@gmail.com>
2024-11-13 19:10:47 +0000

    Used context in components.

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,6 +2,9 @@
 
 import Image from "next/image";
 import Counter from "./counter"
+import ContextCounter from "./context-clicker"
+import {CounterProvider} from './context-counter-provider'
+
 
 
 export default function Home() {
@@ -9,7 +12,11 @@ export default function Home() {
     <div>
       <div>This is correct and should work because a div is really good for this task.</div>
       <Image src='/vercel.svg' alt='' width='30' height='30'/>
-      counter
+      <Counter />
+      <CounterProvider name="Joe">
+      <div>This is a context counter, using a context.</div>
+      <ContextCounter />
+      </CounterProvider>
     </div>
   );
 }
